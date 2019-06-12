cc3f995ce119922f18ef2d4658a13c7e6e580181
pierrejacob <pierre.jacob@gmail.com>
2019-06-12 08:54:31 -0400

    fixed normal coupling

--- 2018-08-gganimate-normal-coupling.R
+++ 2018-08-gganimate-normal-coupling.R
@@ -3,33 +3,34 @@ library(gganimate)
 set.seed(1)
 
 # normal means
-mu1 <- 0
+mu1 <- -1
 mu2 <- 2
 # std deviation
-sigma <- 1
+sigma <- .7
 # number of samples
 nsamples <- 1000
 # reflection-maximal coupling
 reflmax_samples <- matrix(nrow = nsamples, ncol = 2)
 # draw x components first
-reflmax_samples[,1] <- rnorm(nsamples)
+xdot <- rnorm(nsamples)
 # this follows the notation of Bou Rabee et al, 2018, roughly
-z <- mu1 - mu2
+z <- (mu1 - mu2) / sigma
 normz <- sqrt(sum(z^2))
 e <- z / normz
 utilde <- runif(nsamples, 0, 1)
-accepts <- (log(utilde) < (dnorm(e * reflmax_samples[,1] + normz, 0, 1, log = TRUE) - dnorm(e*reflmax_samples[,1], log = TRUE)))
-eta <- reflmax_samples[,1]
-eta[accepts] <- reflmax_samples[accepts,1] + z
-eta[!accepts]<- reflmax_samples[!accepts,1] - 2 * (e * reflmax_samples[!accepts,1]) * e
-reflmax_samples[,2] <- mu2 + eta
+accepts <- (log(utilde) < (dnorm(xdot + z, 0, 1, log = TRUE) - dnorm(xdot, log = TRUE)))
+ydot <- rep(0, nsamples)
+ydot[accepts] <- (xdot)[accepts] + z
+ydot[!accepts]<- xdot[!accepts] - 2 * (e * xdot[!accepts]) * e
+reflmax_samples[,1] <- mu1 + sigma * xdot
+reflmax_samples[,2] <- mu2 + sigma * ydot
 
 df <- data.frame(coupling = rep("reflection-maximal", nsamples), x = reflmax_samples[,1], y = reflmax_samples[,2])
 
 # reflection coupling
 refl_samples <- matrix(0, nrow = nsamples, ncol = 2)
 refl_samples[,1] <- reflmax_samples[,1]
-refl_samples[,2] <- (mu2-mu1)-refl_samples[,1]
+refl_samples[,2] <- mu2-(refl_samples[,1] - mu1)
 
 df <- rbind(df, data.frame(coupling = rep("reflection", nsamples), x = refl_samples[,1], y = refl_samples[,2]))
 
@@ -43,9 +44,9 @@ df <- rbind(df, data.frame(coupling = rep("optimal transport", nsamples), x = tr
 # max coupling
 max_samples <- matrix(0, nrow = nsamples, ncol = 2)
 max_samples[,1] <- reflmax_samples[,1]
-dp <- function(x) dnorm(x, mean = mu1, sd = 1, log = TRUE)
-dq <- function(x) dnorm(x, mean = mu2, sd = 1, log = TRUE)
-rq <- function(n) rnorm(n, mean = mu2, sd = 1)
+dp <- function(x) dnorm(x, mean = mu1, sd = sigma, log = TRUE)
+dq <- function(x) dnorm(x, mean = mu2, sd = sigma, log = TRUE)
+rq <- function(n) rnorm(n, mean = mu2, sd = sigma)
 for (isample in 1:nsamples){
   x <- max_samples[isample,1]
   if (dp(x) + log(runif(1)) < dq(x)){
@@ -63,15 +64,15 @@ for (isample in 1:nsamples){
 df <- rbind(df, data.frame(coupling = rep("maximal", nsamples), x = max_samples[,1], y = max_samples[,2]))
 
 ## Scatter plots and marginals
-ggplot(df, aes(x=x, y=y, group = coupling, colour = factor(coupling))) +
-  geom_point()+
-  theme_minimal() + viridis::scale_color_viridis(discrete=T)
+# ggplot(df, aes(x=x, y=y, group = coupling, colour = factor(coupling))) +
+#   geom_point()+
+#   theme_minimal() + viridis::scale_color_viridis(discrete=T)
 #
-ggplot(df, aes(x=x, group = coupling, fill = factor(coupling))) + geom_histogram(aes(y = ..density..), position = position_dodge()) +
-  theme_minimal() + viridis::scale_fill_viridis(discrete=T) + stat_function(fun = function(x) dnorm(x, mu1, sigma))
-#
-ggplot(df, aes(x=y, group = coupling, fill = factor(coupling))) + geom_histogram(aes(y = ..density..), position = position_dodge()) +
-  theme_minimal() + viridis::scale_fill_viridis(discrete=T) + stat_function(fun = function(x) dnorm(x, mu2, sigma))
+# ggplot(df, aes(x=x, group = coupling, fill = factor(coupling))) + geom_histogram(aes(y = ..density..), position = position_dodge()) +
+#   theme_minimal() + viridis::scale_fill_viridis(discrete=T) + stat_function(fun = function(x) dnorm(x, mu1, sigma))
+# #
+# ggplot(df, aes(x=y, group = coupling, fill = factor(coupling))) + geom_histogram(aes(y = ..density..), position = position_dodge()) +
+#   theme_minimal() + viridis::scale_fill_viridis(discrete=T) + stat_function(fun = function(x) dnorm(x, mu2, sigma))
 
 ## gganimate
 ggplot(df, aes(x = x, y = y)) + xlim(-4, 4) + ylim(-5, 6) +
